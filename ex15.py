5f56d4941211dc0c96ea5f963614249fac637163
가은 <rkdms8228@naver.com>
2022-07-22 13:29:47 +0900

    ex15

---
+++ ex15.py
@@ -0,0 +1,15 @@
+import myMath
+from myMath import plus as pl, minus as mi
+from myMath import pi
+from math import pi
+
+
+print(pl(7,5))
+print(mi(7,5))
+'''
+print(multi(7,5))
+print(div(7,5))
+print(area_circle(7))
+'''
+
+print(pi)
